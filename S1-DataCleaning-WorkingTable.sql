b672ba71c14adcd0e22effe27df60d6c4dd2e92c
Mateen Ilyas <Mateen_Raja2001@yahoo.com>
2024-10-17 11:01:26 +0500

    Update S1-DataCleaning-WorkingTable.sql

--- S1-DataCleaning-WorkingTable.sql
+++ S1-DataCleaning-WorkingTable.sql
@@ -1,16 +1,14 @@
-## Performing the Step 1: Creating a separate table
+-- Performing the Step 1: Creating a separate table
 
-#### Created a separate table `layoffs_staging` exactly like the `layoffs` table
+-- Created a separate table `layoffs_staging` exactly like the `layoffs` table
 CREATE TABLE layoffs_staging
 LIKE layoffs;
-<br /><br />
 
-#### Displaying the empty `layoffs_staging` table to confirm whether it was created or not
+-- Displaying the empty `layoffs_staging` table to confirm whether it was created or not
 SELECT *
 FROM world_layoffs.layoffs_staging;
-<br /><br />
 
-#### Pick all records from `layoffs` table and Insert them into corresponding columns of the `layoffs_staging` table
+-- Pick all records from `layoffs` table and Insert them into corresponding columns of the `layoffs_staging` table
 INSERT layoffs_staging
 SELECT *
 FROM world_layoffs.layoffs;
